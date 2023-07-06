d2233d2d42b8b8c87abd78b75f6efdc7b9ae3885
IordachePavel <138784190+IordachePavel@users.noreply.github.com>
2023-07-06 15:17:32 +0300

    senzor_test-500__Sender_.ino

--- senzor_test-500__Sender_.ino
+++ senzor_test-500__Sender_.ino
@@ -6,11 +6,6 @@ float potval =0 ;
 void setup() {
   Serial.begin(9600);
 
-  // Init the sensor
-  // - Heater control on pin 2
-  // - Sensor analog read on pin A0
-  // - Model LOW_CONCENTRATION
-  // - Load resistance RL of 1MOhms (1000000 Ohms)
   MQ131.begin(8,A2, LOW_CONCENTRATION, 1000000);  
   MQ131.calibrate();
   Serial.print("This is a test");
@@ -19,13 +14,13 @@ void setup() {
   Serial.print(MQ131.getTimeToRead());
   pinMode(enablePin, OUTPUT);
   delay(3000);
-  digitalWrite(enablePin, HIGH);  //  (always high as Master Writes data to Slave)
+  digitalWrite(enablePin, HIGH);  
 }
 
 void loop() {
   MQ131.sample();
   potval=MQ131.getO3(MG_M3);
   Serial.print("Sent Value= ");
-  Serial.println(potval);   //Serial Write POTval to RS-485 Bus
+  Serial.println(potval);  
   delay(10000);
 }
