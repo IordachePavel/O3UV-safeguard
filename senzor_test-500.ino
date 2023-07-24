9217057e52e781e77c1abe0bb93063f3386026b5
IordachePavel <138784190+IordachePavel@users.noreply.github.com>
2023-07-24 16:39:14 +0300

    Update senzor_test-500.ino

--- senzor_test-500.ino
+++ senzor_test-500.ino
@@ -2,7 +2,7 @@
 
 int enablePin = 9;  
 float potval =0 ;
-
+int TareVal=0;
 void setup() {
   Serial.begin(9600);
 
@@ -11,15 +11,24 @@ void setup() {
   MQ131.calibrate();
   Serial.print(MQ131.getR0());
   Serial.print(MQ131.getTimeToRead());
+  pinMode(A5, INPUT);
   pinMode(enablePin, OUTPUT);
   delay(3000);
   digitalWrite(enablePin, HIGH);
+  TareVal=analogRead(A3);
 }
 
 void loop() {
   MQ131.sample();
   potval=MQ131.getO3(MG_M3);
+  if(potval){
   Serial.print("Sent Value= ");
   Serial.println(potval);   
   delay(9600);
+  }
+  if(analogRead(A3)<TareVal-10){
+    Serial.println(9);
+  }
+  else
+  Serial.println(10);
 }
